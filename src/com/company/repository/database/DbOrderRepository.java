b9f738233bba9c5724c4ecc5baeff81cc6396b06
artoy <artoym17mel@tut.by>
2021-08-27 23:00:43 +0300

    Added private method to acquire data and built an order from them u know, but i was doing it under the external influence ;)

--- src/com/company/repository/database/DbOrderRepository.java
+++ src/com/company/repository/database/DbOrderRepository.java
@@ -6,6 +6,7 @@ import com.company.repository.OrderRepository;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DbOrderRepository extends DbAbstractRepository implements OrderRepository {
@@ -20,9 +21,13 @@ public class DbOrderRepository extends DbAbstractRepository implements OrderRepo
 
     public static final String FIND_ORDERS_BY_ID = "select * from orders o join stores s on o.store_id = s.id " +
             "join users u on o.user_id = u.id join roles r on u.role_id = r.id join addresses a on a.id = s.address_id" +
-            "join cities c on c.id = s.city_id";
+            "join cities c on c.id = s.city_id where o.id = ?";
 
-    public static final String FIND_ORDER_BOOK_BY_ID = "";
+    public static final String SELECT_BOOKS_BY_ORDER_ID = "select * from books b join order_book o_k on b.id = book_id " +
+            "join authors a on b.author_id = a.id where o_k.order_id = ?";
+
+    public static final String SELECT_STORE = "select * from stores s join orders o on s.id = o.store_id join cities c on s.city_id = c.id " +
+            "where s.id = ?";
 
 
     @Override
@@ -94,6 +99,33 @@ public class DbOrderRepository extends DbAbstractRepository implements OrderRepo
 
     @Override
     public Order findById(int id) {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(FIND_ORDERS_BY_ID);
+            preparedStatement.setInt(1, id);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            resultSet.next();
+            int orderId = resultSet.getInt(1);
+
+            boolean isDeliveryFromDb = resultSet.getBoolean(4);
+
+            User userFromDb = getResultUser(resultSet);
+
+            PreparedStatement preparedStatement1 = connection.prepareStatement(SELECT_BOOKS_BY_ORDER_ID);
+            preparedStatement1.setInt(1, id);
+
+            ResultSet resultSet1 = preparedStatement1.executeQuery();
+            List<Book> bookList = getResultBook(resultSet1);
+            Book[] books = bookList.toArray(new Book[0]);
+
+            if (isDeliveryFromDb) {
+                return getOrderWithDelivery(resultSet, orderId, isDeliveryFromDb, userFromDb, books);
+            } else {
+                return getOrderWithOutDelivery(resultSet, orderId, userFromDb, books, isDeliveryFromDb);
+            }
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
         return null;
     }
 
@@ -112,7 +144,7 @@ public class DbOrderRepository extends DbAbstractRepository implements OrderRepo
         return null;
     }
 
-    private User resultUser(ResultSet resultSet) {
+    private User getResultUser(ResultSet resultSet) {
         try {
 
             int userId = resultSet.getInt(10);
@@ -123,6 +155,82 @@ public class DbOrderRepository extends DbAbstractRepository implements OrderRepo
             String role = resultSet.getString(17);
 
             return new User(userId, lastName, firstName, email, password, Role.valueOf(role));
+
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+        return null;
+    }
+
+    private List<Book> getResultBook(ResultSet resultSet) {
+        List<Book> bookList = new ArrayList<>();
+
+        try {
+
+            while (resultSet.next()) {
+                int idFromBD = resultSet.getInt(1);
+                String titleFromBd = resultSet.getString(2);
+                String descriptionFromDb = resultSet.getString(3);
+                double priceFromDb = resultSet.getDouble(5);
+                int authorIdFromDb = resultSet.getInt(4);
+                String nickNameFromDb = resultSet.getString(9);
+
+                Author authorFromDb = new Author(authorIdFromDb, nickNameFromDb);
+                Book bookFromDb = new Book(idFromBD, titleFromBd, descriptionFromDb, authorFromDb, priceFromDb);
+                bookList.add(bookFromDb);
+            }
+            return bookList;
+
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+        return null;
+    }
+
+    private Order getOrderWithDelivery(ResultSet resultSet, int orderId, boolean isDelivery, User user, Book[] bookArray) {
+        try {
+
+            String addressFromDb = resultSet.getString(19);
+            Address address = new Address(addressFromDb);
+
+            Order order = new Order(bookArray, user, address, isDelivery);
+
+            return order;
+
+        } catch (SQLException sqlException) {
+            sqlException.printStackTrace();
+        }
+        return null;
+    }
+
+    private Order getOrderWithOutDelivery(ResultSet resultSet, int orderId, User user, Book[] bookArray, boolean isDelivery) {
+        try {
+
+            int storeIdFromDb = resultSet.getInt(5);
+            PreparedStatement preparedStatement = connection.prepareStatement(SELECT_STORE);
+            preparedStatement.setInt(1, storeIdFromDb);
+
+            ResultSet resultSet1 = preparedStatement.executeQuery();
+            resultSet1.next();
+
+            String storeNameFromDb = resultSet1.getString(2);
+
+            int addressIdFromDb = resultSet1.getInt(3);
+            String addressNameFromDb = resultSet1.getString(7);
+
+            int cityIdFromDb = resultSet1.getInt(10);
+            String cityNameFromDb = resultSet1.getString(11);
+
+            Address address = new Address(addressIdFromDb, addressNameFromDb);
+            City city = new City(cityIdFromDb, cityNameFromDb);
+
+            Store store = new Store(storeIdFromDb, storeNameFromDb, address, city);
+
+            Order order = new Order(bookArray, user, isDelivery, store);
+
+            preparedStatement.close();
+
+            return order;
 
         } catch (SQLException sqlException) {
             sqlException.printStackTrace();
